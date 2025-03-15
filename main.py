c0bc12d678714b6bdd71eca1ebe7629431a39ad1
ernestgovaerts <ernest.govaerts2004@gmail.com>
2025-03-15 19:14:43 +0100

    final

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ import pandas as pd
 
 # Experiment metadata en dialoogvenster
 expName = 'Experiment-P0M27a'
-expInfo = {'Participant': '', 'Sessie': ''}
+expInfo = {'Participant': ''}
 dlg = gui.DlgFromDict(dictionary=expInfo, sortKeys=False, title=expName)
 if not dlg.OK:
     core.quit()
 
-# Toewijzing aan een conditie (1:Go-first of 2:No-Go-first)
+# Toewijzing aan een conditie (1:Go-eerst of 2:No-Go-eerst)
 condition = 1 if int(expInfo["Participant"]) % 2 == 0 else 2
 expInfo["assigned_condition"] = condition
 
 # Locaties data initialiseren
 data_dir = "data"
 os.makedirs(data_dir, exist_ok=True)
-file_name_exp = f"p{int(expInfo['Participant']):02d}s{int(expInfo['Sessie'])}"
+file_name_exp = f"p{int(expInfo['Participant']):02d}"
 file_path_exp = os.path.join(data_dir, f"{file_name_exp}.csv")
-file_name_mc = f"mc_p{int(expInfo['Participant']):02d}s{int(expInfo['Sessie'])}.csv"
+file_name_mc = f"mc_p{int(expInfo['Participant']):02d}.csv"
 file_path_mc = os.path.join(data_dir, file_name_mc)
 
 # Functies voor het laden en randomiseren van trial-bestanden
@@ -44,7 +44,7 @@ win = visual.Window(fullscr=True, color="#242424", units="pix", checkTiming=Fals
 
 def show_instructions(text):
     """Toont een instructietekst en wacht tot de deelnemer op [F] drukt om verder te gaan."""
-    visual.TextStim(win, text=text, color="white", wrapWidth=1000, height=35).draw()
+    visual.TextStim(win, text=text, color="white", wrapWidth=1000, height=25).draw()
     win.flip()
     event.waitKeys(keyList=["f"])
 
@@ -53,21 +53,24 @@ def extract_emotion(image_name):
     return "Blij" if image_name[4].upper() == "H" else "Verdrietig"
 
 # Lijst om trial-data in op te slaan
-data_records = []
+data_exp_records = []
+data_mc_records = []
 
 def save_data():
     """Slaat alle verzamelde data op in een CSV-bestand."""
-    pd.DataFrame(data_records).to_csv(file_path_exp, index=False)
-    print(f"Data opgeslagen in {file_path_exp}")
+    pd.DataFrame(data_exp_records).to_csv(file_path_exp, index=False)
+    pd.DataFrame(data_mc_records).to_csv(file_path_mc, index=False)
+    print(f"Experimentele data opgeslagen in {file_path_exp}")
+    print(f"Manipulatiecheck-data opgeslagen in {file_path_mc}")
 
-# Moeilijkheidsrating
-def ask_question(question_text, options):
+# Manipulatiecheck vragen
+def ask_question(question_text, labels, ID):
     question = visual.TextStim(win, text=question_text, color="white", height=35, wrapWidth=800, pos=(0, 200),
                                font="Arial")
-    slider = visual.Slider(win, ticks=options, labels=[str(option) for option in options],
+    slider = visual.Slider(win, ticks=[1,2,3,4,5], labels=labels,
                            granularity=1, style=["rating"], size=(600, 40), pos=(0, 0), color="white",
                            labelColor="white", font="Arial")
-    instruction = visual.TextStim(win, text="Druk op [F] om verder te gaan", color="white", height=25, pos=(0, -200))
+    instruction = visual.TextStim(win, text="Druk op [F] om verder te gaan", color="white", height=25, pos=(0, -200), font="Arial")
 
     response = None
     while response is None:
@@ -82,14 +85,11 @@ def ask_question(question_text, options):
         elif "escape" in keys:
             core.quit()
 
-    data_record = pd.DataFrame([{
+    data_mc_records.append({
         "Participant": expInfo["Participant"],
-        "Session": expInfo["Sessie"],
-        "DifficultyRating": response
-    }])
-
-    data_record.to_csv(file_path_mc, index=False)
-    print(f"Data opgeslagen in {file_path_mc}")
+        "ID": ID,
+        "Response": response
+    })
 
 def run_trials(trial_data, block_number):
     """Voert de trials uit voor een blok en registreert de response en reactietijden.
@@ -152,9 +152,8 @@ def run_trials(trial_data, block_number):
             core.wait(1.5)
 
         # Sla trialdata op
-        data_records.append({
+        data_exp_records.append({
             "Participant": expInfo["Participant"],
-            "Session": expInfo["Sessie"],
             "Block": block_number,
             "Image": img_path,
             "ISI": isi_duration,
@@ -170,25 +169,41 @@ def run_trials(trial_data, block_number):
             save_data()
             core.quit()
 
-
-
-#question = visual.TextStim(win, text="Hoe moeilijk vond je deze taak?", color="white", height=35, wrapWidth=800, pos=(0, 200), font="Arial")
-#slider = visual.Slider(win, ticks=[1, 2, 3, 4, 5], labels=["Heel makkelijk", "Makkelijk", "Gemiddeld", "Moeilijk", "Heel moeilijk"],
-#                        granularity=1, style=["rating"], size=(600, 40), pos=(0, 0), color="white", labelColor="white", font="Arial")
-
-
-ask_question("Hoe moeilijk vond je deze taak?", [1,2,3,4,5,6])
 # Experiment flow met instructies en trials
-show_instructions("Welkom bij het experiment. \n\n Druk op [F] om verder te gaan.")
 show_instructions(
-    f"In dit deel zijn {'BLIJE' if condition == 1 else 'VERDRIETIGE'} gezichten de Go-stimuli \n en {'VERDRIETIGE' if condition == 1 else 'BLIJE'} gezichten de No-Go-stimuli. \n\n Druk op [F] om verder te gaan."
+    "Welkom!.\n\n"
+    "In dit experiment zal je blije en verdrietige gezichten te zien krijgen.\n"
+    "Op het volgende scherm zie je in welke conditie je zit. "
+    "Daar wordt aangegeven welke gezichtsuitdrukking de go-stimulus is (waarop je moet reageren) en "
+    "welke de no-go-stimulus is (waarop je niet mag reageren). "
+    "Het is belangrijk dat je dit goed onthoudt.\n\n"
+    "Het experiment bestaat uit zes blokken, verdeeld over twee delen van drie blokken. \n"
+    "Na het eerste deel beantwoord je een vraag over de taak, gevolgd door een korte pauze van 30 seconden. "
+    "Daarna wisselen de gezichtsuitdrukkingen (blij/verdrietig) waarvoor je moet reageren (go) en niet mag reageren (no-go) van rol.\n\n"
+    "Voor elk deel krijg je eerst een korte oefenronde waarin je kunt wennen aan de taak. "
+    "Als je een fout maakt, krijg je kort feedback. Bij een juist antwoord krijg je geen feedback.\n\n"
+    "Druk op [F] om verder te gaan."
 )
-show_instructions("Oefenronde 1 begint nu. \n\n Druk op [F] om verder te gaan.")
+
+show_instructions(
+    f"In dit deel zijn {'BLIJE' if condition == 1 else 'VERDRIETIGE'} gezichten de Go-stimuli. "
+    f"Dit betekent dat je op de spatiebalk moet drukken wanneer je deze gezichten ziet.\n\n"
+    f"{'VERDRIETIGE' if condition == 1 else 'BLIJE'} gezichten zijn de No-Go-stimuli. "
+    f"Je mag hier niet op reageren.\n\n"
+    "Druk op [F] om verder te gaan."
+)
+show_instructions("Oefenronde 1. \n\n Druk op [F] om te starten.")
 run_trials(first_practice, "Practice 1")
 
 for i in range(3):
-    show_instructions(f"Blok {i + 1} begint nu. \n\n Druk op [F] om verder te gaan.")
+    show_instructions(f"Blok {i + 1}. \n\n Druk op [F] om te starten")
     run_trials(blocks[i], i + 1)
+
+# Eerste manipulatiecheck
+if condition == 1:
+    ask_question("Hoe vaak heb je op de spatiebalk gedrukt bij een blije gezichtsuitdrukking?", ["Bijna nooit of nooit", "Zelden", "Soms", "Vaak", "Bijna altijd of altijd"], ID="mc1")
+else:
+    ask_question("Hoe vaak heb je op de spatiebalk gedrukt bij een verdrietige gezichtsuitdrukking?", ["Bijna nooit of nooit", "Zelden", "Soms", "Vaak", "Bijna altijd of altijd"], ID="mc2")
 
 # Pauze van 30 seconden tussen de twee delen van het experiment
 show_instructions(
@@ -199,15 +214,36 @@ core.wait(30)
 show_instructions(
     f"Vanaf nu zijn {'VERDRIETIGE' if condition == 1 else 'BLIJE'} gezichten de Go-stimuli \n en {'BLIJE' if condition == 1 else 'VERDRIETIGE'} gezichten de No-Go-stimuli. \n\n Druk op [F] om verder te gaan."
 )
-show_instructions("Oefenronde 2 begint nu. \n\n Druk op [F] om verder te gaan.")
+show_instructions("Oefenronde 2. \n\n Druk op [F] om te starten.")
 run_trials(second_practice, "Practice 2")
 
 for i in range(3, 6):
-    show_instructions(f"Blok {i + 1} begint nu. \n\n Druk op [F] om verder te gaan.")
+    show_instructions(f"Blok {i + 1}. \n\n Druk op [F] om te starten.")
     run_trials(blocks[i], i + 1)
+
+show_instructions(
+    "Je hebt alle blokken doorlopen. \n\n"
+    "Voor we het experiment afsluiten, moet je nog kort twee vragen beantwoorden.\n"
+    "Deze vragen gaan over de taak die je net hebt uitgevoerd. \n\n"
+    "Druk op [F] om verder te gaan."
+)
+# Tweede manipulatiecheck
+if condition == 2:
+    ask_question("Hoe vaak heb je op de spatiebalk gedrukt bij een blije gezichtsuitdrukking?", ["Bijna nooit of nooit", "Zelden", "Soms", "Vaak", "Bijna altijd of altijd"], ID="mc1")
+else:
+    ask_question("Hoe vaak heb je op de spatiebalk gedrukt bij een verdrietige gezichtsuitdrukking?", ["Bijna nooit of nooit", "Zelden", "Soms", "Vaak", "Bijna altijd of altijd"], ID="mc2")
+
+# Derde en vierde manipulatiecheck
+ask_question("De reactietijd die ik had om te reageren op de stimulus was:", ["Altijd hetzelfde", "Meestal hetzelfde, met enkele uitzonderingen", "Sterk variërend tussen trials"], ID="mc3")
+ask_question("Het verschil tussen de gezichtsuitdrukkingen was duidelijk.", ["Niet akkoord", "Neutraal", "Akkoord"], ID="mc4")
+
 
 # Data opslaan en experiment afsluiten
 save_data()
-show_instructions("Experiment voltooid. \n\n Bedankt voor je deelname!")
+show_instructions(
+    "Bedankt voor je deelname! \n\n"
+    "Je antwoorden en reacties zullen anoniem worden verwerkt en geanalyseerd.\n\n"
+    "Je mag nu de testleider verwittigen dat je klaar bent.\n\n"
+)
 win.close()
 core.quit()
---
+++ questions.md
@@ -0,0 +1,9 @@
+### Manipulatiechecks
+Check mc1: Hoe vaak heb je op de spatiebalk gedrukt bij een blije gezichtsuitdrukking? <br>
+    Opties: ["Bijna nooit of nooit",[0] ; "Zelden",[1] ; "Soms", [0] ; "Vaak",[1] ; "Bijna altijd of altijd",[2]] <br><br>
+Check mc1: Hoe vaak heb je op de spatiebalk gedrukt bij een verdrietige gezichtsuitdrukking? <br>
+    Opties: ["Bijna nooit of nooit",[0] ; "Zelden",[1] ; "Soms", [0] ; "Vaak",[1] ; "Bijna altijd of altijd",[2]] <br><br>
+Check mc3: De reactietijd die ik had om te reageren op de stimulus was: <br>
+    Opties: ["Altijd hetzelfde",[0] ; "Meestal hetzelfde, met enkele uitzonderingen",[1] ; "Sterk variërend tussen trials", [2]] <br><br>
+Check mc4: Het verschil tussen de gezichtsuitdrukkingen was duidelijk. <br>
+    Opties: ["Niet akkoord",[0] ; "Neutraal",[1] ; "Akkoord",[2]] <br><br>
